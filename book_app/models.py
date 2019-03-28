b5c5bf763d3e73f1d250841870e0cf413adb02de
Paul Williamsen <pwilliamsen@me.com>
2019-03-28 07:47:50 -0700

    models

--- book_app/models.py
+++ book_app/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 
 class Book(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='books')
     title = models.CharField(max_length=48)
     author = models.CharField(max_length=4096)
     year = models.CharField(max_length=4096)
