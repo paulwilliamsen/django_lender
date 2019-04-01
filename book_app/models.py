bd2f09aa7957af117f0979d4f637a8a89aaa53c6
Paul Williamsen <pwilliamsen@me.com>
2019-04-01 06:43:55 -0700

    auth work done

--- book_app/models.py
+++ book_app/models.py
@@ -1,22 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.timezone import now
 
 
 # Create your models here.
+
+STATES = [
+    ('Available', 'Available'),
+    ('Checked Out', 'Checked Out')
+    ]
+
 
 class Book(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='books')
     title = models.CharField(max_length=48)
     author = models.CharField(max_length=4096)
     year = models.CharField(max_length=4096)
-    date_added = models.DateTimeField(auto_now_add=True, blank=True)
-    last_borrowed = models.DateTimeField(auto_now=True, blank=True)
-
-    STATES = [
-        ('Available', 'Available'),
-        ('Checked Out', 'Checked Out')
-    ]
-
+    date_added = models.DateTimeField(default=now)
+    last_borrowed = models.DateTimeField(auto_now=True)
     status = models.CharField(choices=STATES, default='available', max_length=48)
 
     def __repr__(self):
@@ -24,3 +25,4 @@ class Book(models.Model):
 
     def __str__(self):
         return f'{ self.title } | Status: { self.status }'
+
